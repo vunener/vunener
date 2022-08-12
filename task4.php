1505767d0c3a24a127324642421c0e0b4fef57fc
vunener <110974557+vunener@users.noreply.github.com>
2022-08-12 16:19:06 +0200

    Update task4.php

--- task4.php
+++ task4.php
@@ -1,4 +1,17 @@
-<?php include 'menu.inc'; ?>
+<?php include 'menu.inc'; 
+
+$query = "SELECT code, name, lecturer FROM modules";
+$result = $conn->query($query);
+while($data = $result->fetch (PDO:: database)){
+?>
+<tr>
+    <td><?php echo $data['code'];?></td>
+    <td><?php echo $data['name'];?></td>
+    <td><?php echo $data['lecturer']; ?></td>
+</tr>
+<?php
+}
+?>
 <iframe src="task4.txt" height="400" width="1200">
     Your browser does not support iframes.
 </iframe>
